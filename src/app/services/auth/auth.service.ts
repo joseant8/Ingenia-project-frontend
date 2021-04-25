11214506d913e17e168026992ebfa02da405361d
Jose Antonio <j2a2mari@gmail.com>
2021-04-26 00:36:44 +0200

    handle error request

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { Observable, of, Subject } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http'
+import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user/user.model';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -19,7 +21,23 @@ export class AuthService {
       password: user.password
     };
     // http siempre devuelve observables
-    return this.http.post(this.LOGIN_URL, body);
+    return this.http.post(this.LOGIN_URL, body).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+          // client-side error
+          errorMessage = `Error: ${error.error.message}`;
+        } else {
+          // server-side error
+          if(error.status === 401){  // 401: Unauthorized
+            errorMessage = 'No autorizado. El email y/o contraseña no son válidos.';
+          }
+
+        }
+        window.alert(errorMessage);
+        return throwError(errorMessage);
+      })
+    );
   }
 
   logout(){
