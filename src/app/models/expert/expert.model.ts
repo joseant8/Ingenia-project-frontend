90063936ebcf0fb5187d1cfb57dffef17f15f59c
Jose Antonio <j2a2mari@gmail.com>
2021-04-26 12:39:52 +0200

    add new attributes to expert model

--- src/app/models/expert/expert.model.ts
+++ src/app/models/expert/expert.model.ts
@@ -5,16 +5,34 @@ export class Expert implements IExpert{
   id:number;
   nombre:string;
   created_at:Date;
+  updated_at:Date;
+  nif: string;
   estado:string;
+  disponibilidad: string;
   puntuacion: number;
+  origen: string;
+  contacto_telefono: string;
+  contacto_email: string;
+  fichero_cv: string;
+  contacto_linkedin: string;
   etiquetas: Tag[];
 
-  constructor(id:number, nombre:string, created_at:Date, estado:string, puntuacion:number, etiquetas: Tag[]) {
+  constructor(id:number, nombre:string, created_at:Date, updated_at:Date, nif: string, estado:string, disponibilidad: string,
+    puntuacion:number, origen: string, contacto_telefono: string, contacto_email: string, fichero_cv: string, contacto_linkedin: string,
+    etiquetas: Tag[]) {
     this.id = id;
     this.nombre = nombre;
     this.created_at = created_at;
+    this.updated_at = updated_at;
+    this.nif = nif;
     this.estado = estado;
+    this.disponibilidad = disponibilidad;
     this.puntuacion = puntuacion;
+    this.origen = origen;
+    this.contacto_telefono = contacto_telefono;
+    this.contacto_email = contacto_email;
+    this.fichero_cv = fichero_cv;
+    this.contacto_linkedin = contacto_linkedin;
     this.etiquetas = etiquetas;
   }
 }
--- src/app/models/expert/iexpert.interface.ts
+++ src/app/models/expert/iexpert.interface.ts
@@ -4,7 +4,15 @@ export interface IExpert {
   id:number,
   nombre:string,
   created_at:Date,
+  updated_at:Date,
+  nif: string,
   estado:string,
+  disponibilidad: string,
   puntuacion: number,
+  origen: string,
+  contacto_telefono: string,
+  contacto_email: string,
+  fichero_cv: string,
+  contacto_linkedin: string,
   etiquetas: Tag[]
 }
