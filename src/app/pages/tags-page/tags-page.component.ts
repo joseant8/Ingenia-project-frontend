c0f32662ca7afb87a485d950191c9f6c6cedc367
Jose Antonio <j2a2mari@gmail.com>
2021-04-26 05:27:00 +0200

    add code and changes

--- src/app/pages/tags-page/tags-page.component.ts
+++ src/app/pages/tags-page/tags-page.component.ts
@@ -12,6 +12,7 @@ export class TagsPageComponent implements OnInit, OnDestroy {
 
   tagsList:Tag[] = [];
   tagSubscription: Subscription = new Subscription();
+  displayedColumns: string[] = ['nombre', 'creador', 'fecha_creacion', 'eliminar'];
 
   constructor(private tagService: TagService) { }
 
@@ -20,6 +21,17 @@ export class TagsPageComponent implements OnInit, OnDestroy {
       this.tagsList = response;
     });
   }
+
+
+  deleteTag(id:number, nombre:string){
+    if(window.confirm('¿Estás seguro que deseas eliminar \'' + nombre + '\' de la lista de etiquetas?\n¡La etiqueta desaparecerá de la ficha de expertos también!')){
+      this.tagService.deleteTag(id).subscribe((response) => {
+        window.location.reload();  // recargar la página
+        alert('Etiqueta \'' + nombre + '\' eliminada correctamente');
+      });
+    }
+  }
+
 
   ngOnDestroy(): void {
     this.tagSubscription.unsubscribe();
