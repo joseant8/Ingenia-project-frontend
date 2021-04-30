a29bd12468e359702cd7f11dbdbcf60170cb9a2b
Jose Antonio <j2a2mari@gmail.com>
2021-04-30 03:19:52 +0200

    add creator model

---
+++ src/app/models/creator/creator.model.spec.ts
@@ -0,0 +1,7 @@
+import { Creator } from './creator.model';
+
+describe('Creator', () => {
+  it('should create an instance', () => {
+    expect(new Creator()).toBeTruthy();
+  });
+});
---
+++ src/app/models/creator/creator.model.ts
@@ -0,0 +1,9 @@
+export class Creator {
+  id: number;
+  nombreCompleto: string;
+
+  constructor(id: number, nombreCompleto: string){
+    this.id = id;
+    this.nombreCompleto = nombreCompleto;
+  }
+}
